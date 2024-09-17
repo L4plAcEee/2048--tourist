b69657d812fa77b32ca96b0f524f1600f4b3e2e9
f10w <1906518568@qq.com>
2024-09-17 15:37:31 +0800

    增加 print

---
+++ print_part-2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct game_play {
+    int game_grid[4][4];
+    long current_score;
+    int game_status;
+    int available;
+} game_play;
+
+game_play game;
+
+int initalize_game() {
+    srand((unsigned)time(NULL));
+    for (size_t row = 0; row < 4; ++row){
+        for (size_t col = 0; col < 4; ++col)
+        {
+            game.game_grid[row][col] = 0;
+        }
+    }
+    game.current_score = 0;
+    game.game_status = 1;
+    game.available = 16;
+    return 1;
+}
+
+void print_game_grid() {
+    printf("gird:\n");
+    for (size_t row = 0; row < 4; ++row){
+        for (size_t col = 0; col < 4; ++col)
+        {
+            printf("%4d",game.game_grid[row][col]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    if(initalize_game) printf("Initalize game success!\n");
+    print_game_grid();
+    return 1;
+}
